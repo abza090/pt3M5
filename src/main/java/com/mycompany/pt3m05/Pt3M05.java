4afdde24f54ba6246f0d02a8feef9a0dc5be6744
abza090 <abza090@vidalibarraquer.net>
2023-06-04 17:50:10 +0200

    first commit

---
+++ src/main/java/com/mycompany/pt3m05/Pt3M05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pt3m05;
+
+/**
+ *
+ * @author odoo
+ */
+public class Pt3M05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
